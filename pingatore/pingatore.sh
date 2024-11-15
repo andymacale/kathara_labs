4b2ce3f8b750d342e70e7b95af97d395e38172cd
andymacale <146097594+andymacale@users.noreply.github.com>
2024-11-15 11:16:58 +0100

    kok

--- pingatore/pingatore.sh
+++ pingatore/pingatore.sh
@@ -1,44 +1,45 @@
 #!/bin/sh
 
-fileC=$(find . -name pingatore.c)
+# Cerca e compila il file pingatore.c
+fileC=$(find . -maxdepth 1 -name "pingatore.c")
 
-if [ -n $fileC]; then
-    gcc -o pingatore pingatore.c
-    wait
-    rm -f pingatore.c
+if [ -n "$fileC" ]; then
+    gcc -o pingatore "$fileC" || { echo "Errore durante la compilazione"; exit 1; }
+    rm -f "$fileC"
 fi
 
+# Gestione del percorso specificato in $1
 if [ -n "$1" ]; then
     pos=$(pwd)
-    cd "$1" || { echo "Percorso '$1\' non trovato"; exit 1; }
-    cd "$pos"
-fi
-
-arg=$(echo $file | xargs)
-
-if [ -n "$1" ];
-    then
-        lista=$(find macaleandrea/*.startup)
-        carattere="/n"
-        carattere=" "
-        file=${lista//$carattere}
-        
+    if cd "$1"; then
+        echo "Percorso cambiato in $1"
+        cd "$pos"
     else
-        lista=$(ls -m | grep .startup)
-        carattere=","
-        file=${lista//$carattere}
+        echo "Percorso '$1' non trovato"
+        exit 1
+    fi
 fi
 
-arg=$(echo $file | xargs)
-
-programma="./pingatore"
-
-comando="${programma} ${arg}"
-
-$comando
-
-if [ -f indirizzi.sh ]; then
-    chmod 777 indirizzi.sh
+# Elaborazione dei file .startup
+if [ -n "$1" ]; then
+    lista=$(find macaleandrea -name "*.startup")
+    carattere="\n"
+    file=$(echo "$lista" | tr -d "$carattere")
 else
-    echo "File 'indirizzi.sh' non trovato!"
+    lista=$(ls -m | grep ".startup")
+    carattere=","
+    file=$(echo "$lista" | tr -d "$carattere")
+fi
+
+# Rimuove spazi in eccesso
+arg=$(echo "$file" | xargs)
+
+# Esegui il programma se compilato
+programma="./pingatore"
+if [ -f "$programma" ]; then
+    echo "Eseguo $programma con argomenti: $arg"
+    $programma $arg
+else
+    echo "Il programma $programma non è stato trovato."
+    exit 1
 fi
