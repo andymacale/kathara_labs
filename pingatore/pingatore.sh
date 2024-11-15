bef0aa8775d14affb5d3907a529a9f0ea3905140
andymacale <146097594+andymacale@users.noreply.github.com>
2024-11-15 10:54:11 +0100

    dwkjhdw

---
+++ pingatore/pingatore.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+if [ -n "$1" ]; then
+    pos=$(pwd)
+    cd "$1" || { echo "Percorso '$1\' non trovato"; exit 1; }
+    cd "$pos"
+fi
+
+arg=$(echo $file | xargs)
+
+if [ -n "$1" ];
+    then
+        lista=$(find macaleandrea/*.startup)
+        carattere="/n"
+        carattere=" "
+        file=${lista//$carattere}
+        
+    else
+        lista=$(ls -m | grep .startup)
+        carattere=","
+        file=${lista//$carattere}
+fi
+
+arg=$(echo $file | xargs)
+
+programma="./pingatore"
+
+comando="${programma} ${arg}"
+
+$comando
+
+if [ -f indirizzi.sh ]; then
+    chmod 777 indirizzi.sh
+else
+    echo "File 'indirizzi.sh' non trovato!"
+fi
